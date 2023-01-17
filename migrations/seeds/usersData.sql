11c6852afb6ca4d964bc687aedf23a1882da14e1
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2023-01-17 18:21:25 -0500

    Not Nav, Database
    - Migrations folder created along with 'schema' (should be renamed if I chose the wrong name scheme) and 'seeds'.
    - The 'schema' folder: CREATE TABLE command
    - The 'seeds' folder: INSERT()VALUE() command
    
    Both files can be easily edited

---
+++ migrations/seeds/usersData.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (
+    fname, lname, uname, email, pswrd
+)
+Values ("John", "Smith",  "jsmith", "john.smith@example.com", "J@sminth23#"),
+("Amy", "Brown", "abrown", "amy.brown@example.com", "AmYbR0wn#"),
+("Michael", "Williams", "mwilliams", "michael.williams@example.com", "M!ch@elW1ll#"),
+("Jessica", "Jones", "jjones", "jessica.jones@example.com", "Jess!caJ0n3s#"),
+("David", "Miller", "dmiller", "david.miller@example.com", "D@v!dM!ll3r#"),
+("Amber", "Davis", "adavis", "amber.davis@example.com", "a@m@bb@D@v!s#");
+
---
+++ server.js
@@ -0,0 +1,17 @@
+console.log("first try")
+const express = require('express')
+const app = express()
+const port = 3030
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res)=>{
+    res.render("landing")
+})
+app.get('/nav', (req,res)=>{
+    res.render("nav")
+}) // -- this can be ignored
+
+app.listen(port, ()=>
+console.log("server running"))
