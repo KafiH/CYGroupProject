9818f3d3877599f9c4e5d99749803a5e58237efe
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2023-01-20 01:27:23 -0500

    Nav Bar Test
    ACCOMPLISHED:
    - FUNCTION: Navigation bar has page links awaiting correlating .ejs files
    - STYLE: Color scheme selected from (landing.ejs)
    - TEST: Light formatting on (landing1.ejs) to accommodate nav.js link
    
    NEXT VERSION:
    - PROFILE ICON: Placeholder 'inner.HTML' of #proCon needs function to pull 'User Initials' from DB
    - SHOP LINK: Placeholder (presentExample.png) page link needs a <div> that displays 'User Achievement' counter
    -  PLAYER SCORE: Requires function to display 'User Score'
    
    FILES CREATED:
    - (nav1.js/nav1.css) created to solve formatting issues.
    - (landing1.ejs) created to test nav1.js file link and page format.
    - (presentExample.png) created as Placeholder image until user DB is linked

--- public/js/nav1.js
+++ public/js/nav1.js
@@ -1,10 +1,13 @@
-// -- * links need to be changed to match .ejs file paths as they are completed *
+
+
+// -- 19-01-2023 basic copy to improve function
+
 
 // ## Head 
 var head = document.querySelector("head");
 var link = document.createElement("link");
 link.rel = "stylesheet";
-href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&family=Roboto&family=Shadows+Into+Light&display=swap"
+href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&family=Roboto&family=Shadows+Into+Light&display=swap";
 head.appendChild(link);
 
 // ## Body
@@ -14,86 +17,41 @@ const  nav = document.createElement('nav');
     nav.id="nav";
 document.body.appendChild(nav);
 
-// -- # Profile Link 
-// -- *  requires code connected to a DB to pull user intitials *
-// -- *  link to sign in page if user === 'Null'  *
+
+
+
+// -- div to format left nav links
 const leftNavGroup = document.createElement('div');
     leftNavGroup.id='leftNavGroup';
 nav.appendChild(leftNavGroup);
+// -- left nav links
 
-
-
-
-
-
-// -- profile icon
+// -- Profile Icon
 const profileIcon = document.createElement('a');
-leftNavGroup.appendChild(profileIcon);
-const profileInitials = document.createElement('p');
-    profileInitials.innerHTML("JD");
+    profileIcon.innerHTML="JD"; 
+    profileIcon.href= "/profile";   
     profileIcon.id="proCon";
-profileIcon.appendChild(profileInitials);    
-
-
-
-// -- Input initials from database code sample
-// const funTest = document.getElementById('funTest').onClick = function(){
-// const { Client } = require('pg');
-
-// async function getInitials(uname) {
-//   const client = new Client();
-//   await client.connect();
-
-//   const query = `SELECT initials FROM users WHERE username = '${uname}'`;
-//   const result = await client.query(query);
-
-//   await client.end();
-  
-//   return result.rows[0].initials;
-// }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// -- # Leader Board Link
-// -- * link to global leader board db to display user score *
-const userScore = document.createElement('a');
-    userScore.innerHTML = "<b>1000 Xp</b>"; // -- placeholder text
-    userScore.href = "/leaderBoard";
-    userScore.id="userScore";
-leftNavGroup.appendChild(userScore);
+leftNavGroup.appendChild(profileIcon);
 
 // -- # Shop Link
 // -- * Would like a 'present' icon with a notification counter *
 const achievLink = document.createElement('a');
     achievLink.href="/shop";
+    achievLink.id="achievLink";
 leftNavGroup.appendChild(achievLink);
 
 const achievements = document.createElement('img');
-    achievements.src="/img/present.png"; // -- placeholder text
+    achievements.src="/img/presentExample.png"; // -- image needs a div with a function that displays the number of 'achievements'
     achievements.link = "/shop";
     achievements.id="achiev"
 achievLink.appendChild(achievements);
+
+// -- Leader board
+const userScore = document.createElement('a');
+    userScore.innerHTML = "<b>1000 Xp</b>"; // -- placeholder text create function that displays from leaderboard
+    userScore.href = "/leaderBoard";
+    userScore.id="userScore";
+leftNavGroup.appendChild(userScore);
 
 // -- # Home Button
 const homeCon = document.createElement('a');
@@ -101,11 +59,12 @@ const homeCon = document.createElement('a');
     homeCon.href="/";
     homeCon.id="homeLink";
 nav.appendChild(homeCon);
-
+// -- right nav container
 const rightNavGroup = document.createElement('div');
     rightNavGroup.id='rightNavGroup';
 nav.appendChild(rightNavGroup);
 
+// -- right nav links
 const about = document.createElement('a');
     about.innerHTML = "About";
     about.href="/about";
