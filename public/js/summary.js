a7e1573ad3043afbf5c45cd843b0bdfc24af18c1
burakcersit <burakcersit@hotmail.com>
2023-01-24 15:08:14 -0500

    summary

--- public/js/summary.js
+++ public/js/summary.js
@@ -3,6 +3,8 @@
 // contentContainer.id='contentContainer';
 // mainCont.appendChild(contentContainer);
 
+
+//getting scrore from local storage
 var introPoint = document.querySelector("#introPoint");
 let localIntro = JSON.parse(localStorage.getItem("scoreList"));
 let intro = (localIntro[0].score)
