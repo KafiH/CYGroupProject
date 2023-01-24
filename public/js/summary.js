c42e793252fc91b6d709de45049cc8875e9ec3b7
burakcersit <burakcersit@hotmail.com>
2023-01-24 15:06:51 -0500

    summary file change

--- public/js/summary.js
+++ public/js/summary.js
@@ -1,5 +1,10 @@
-const mainCont = document.getElementsByTagName('main');
-const contentContainer = document.createElement('div');
-contentContainer.id='contentContainer';
-mainCont.appendChild(contentContainer);
+// const mainCont = document.getElementsByTagName('main');
+// const contentContainer = document.createElement('div');
+// contentContainer.id='contentContainer';
+// mainCont.appendChild(contentContainer);
+
+var introPoint = document.querySelector("#introPoint");
+let localIntro = JSON.parse(localStorage.getItem("scoreList"));
+let intro = (localIntro[0].score)
+introPoint.textContent = (intro + "%");
 
